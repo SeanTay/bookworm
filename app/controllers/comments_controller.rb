f0c7e38e3975ac0f550003282e5be867e9885994
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-07-06 09:03:52 -0400

    css login page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 
+before_action :authenticate_user!
   def index
     @comments = Comment.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
+  get 'books/index'
+
   root to: "books#index"
+
   resources :books do
     resources :comments
   end
